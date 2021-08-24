8bf124dd53f6eb0e27f8c28969c09c77ba9910a4
unknown <skiletro>
2021-08-24 13:50:25 +0100

    yoo

---
+++ script.js
@@ -0,0 +1,14 @@
+VANTA.DOTS({
+  el: "*",
+  mouseControls: false,
+  touchControls: false,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xAB4AFF,
+  color2: 0x121212,
+  backgroundColor: 0x121212,
+  spacing: 32.00
+})
