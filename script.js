492dbb94551b8dfe96e61111bfc74da8a29ba1fd
skilet.ro <19377854+skiletro@users.noreply.github.com>
2022-02-04 21:31:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-VANTA.DOTS({
-  el: "*",
-  mouseControls: false,
-  touchControls: false,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0xAB4AFF,
-  color2: 0x121212,
-  backgroundColor: 0x121212,
-  spacing: 32.00
-})
+VANTA.DOTS({ el: "*", mouseControls: false, touchControls: false, gyroControls: false, minHeight: 200.00, minWidth: 200.00, scale: 1.00, scaleMobile: 1.00, color: 0xAB4AFF, color2: 0x121212, backgroundColor: 0x121212, spacing: 32.00 }) let showaltpage = function() { if ($("#one").is(":visible")) { $("#one").hide(); $("#two").show(); } else { $("#one").show(); $("#two").hide(); } }
